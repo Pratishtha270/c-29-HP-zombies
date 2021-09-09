d534628796b5d86ca5c5893eb5310289ea79c730
Pratishtha270 <82306312+Pratishtha270@users.noreply.github.com>
2021-09-09 17:46:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+var base;
+var leftSide,rightSide;
+var bridgeHolder,bridgeHolder1;
+var bridge;
+var jointPoint,jointLink;
+
+
+function setup() {
+  createCanvas(500,700);
+  engine = Engine.create();
+  world = engine.world;
+  frameRate(80);
+
+  base=new Base(200,690,600,20);
+  leftSide=new Base(10,350,20,700);
+  rightSide=new Base(490,350,20,700);
+  bridgeHolder=new Base(20,350,150,70);
+  bridgeHolder1=new Base(480,350,150,70);
+  bridge=new Bridge(9,{x:70,y:340});
+  jointPoint=new Base(405,350,10,70);
+
+  Matter.Composite.add(bridge.body,jointPoint);
+  jointLink=new Link(bridge,jointPoint);
+
+}
+
+function draw() {
+  background(51);
+  Engine.update(engine);
+
+  base.show();
+  rightSide.show();
+  leftSide.show();
+  bridgeHolder.show();
+  bridgeHolder1.show();
+  bridge.show();
+  jointPoint.show();
+
+}
